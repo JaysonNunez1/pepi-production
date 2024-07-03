4e011923804fd055db494b2c42ad8ac14ce443a2
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-07-03 10:11:20 -0400

    worked on the hero img

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,19 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-
-
+import logo from '../assets/pepi-logo1.jpeg';
+import hero from '../assets/pepi1.jpg';
 function Homepage() {
     return (
         <div className="homepage"> 
         <header>
+        <img src={logo} alt="Pepi Production Logo" width="290px" />
             <h1>Pepi Production</h1>
             <p>Photobooth & Dj Services</p>
         </header>
         <section className="hero">
-            <img src="" alt="Hero Image"/>
+            <img src={hero} alt="Hero Image"/>
             <div className="overlay">
-                <h2>Capture the Moment</h2>
-                <p>Photobooth & Dj Services</p>
+                <h2></h2>
+                <p></p>
             </div>
         </section>
         <section className="services">
